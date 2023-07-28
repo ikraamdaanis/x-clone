81cbd4d10ed5c97b138319efd3e98c69f61b5eed
Ikraam <ibndaanis@gmail.com>
2023-07-28 01:56:56 +0100

    basic styling and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import { Logo } from "@/components/Logo";
-
 export default function Home() {
   return (
-    <main className="bg-white">
-      <Logo />
-    </main>
+    <div className="flex h-full w-full items-center justify-center bg-black">
+      <div className="relative flex h-full w-full max-w-screen-lg items-center">
+        <header>Sidebar</header>
+        <main className="flex">
+          <section>Timeline</section>
+          <section>Aside</section>
+        </main>
+      </div>
+    </div>
   );
 }
